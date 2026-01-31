d8d945be170cf03fc724b46cb4b83642dca056ac
tahirshaikhs <114974768+tahirshaikhs@users.noreply.github.com>
2026-01-31 20:41:11 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,29 +1,5 @@
-# app/config.py
-
-# ==============================
-# TELEGRAM SETTINGS
-# ==============================
-
 BOT_TOKEN = "8099434896:AAFwFQk1qChv_Jxb2VhvXR8u8_E4TIs1_og"
-# example: "123456789:AAHxxxxxx"
-
 CHANNEL = "@desidekhoo"
-# example: "@desidekhoo"
-
-
-# ==============================
-# INSTAGRAM SETTINGS
-# ==============================
 
 INSTAGRAM_USERNAME = "rohit800mehra8"
-# example: "rohit800mehra8"
-
-# Path to instaloader session file
 SESSION_FILE = "session/session-rohit800mehra8"
-
-
-# ==============================
-# LIMITS
-# ==============================
-
-POSTS_PER_HOUR = 30

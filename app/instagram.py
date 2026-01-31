45650941d85b3d31915fadbd57ca8ea6e8676935
tahirshaikhs <114974768+tahirshaikhs@users.noreply.github.com>
2026-01-31 20:21:01 +0530

    Update instagram.py

--- app/instagram.py
+++ app/instagram.py
@@ -1,28 +1,23 @@
+# app/instagram.py
 import instaloader
 from .config import SESSION_FILE, INSTAGRAM_USERNAME
 
 L = instaloader.Instaloader(
     download_pictures=False,
     download_videos=False,
-    download_video_thumbnails=False,
     download_comments=False,
     save_metadata=False,
+    quiet=True,
 )
 
-# Load session
-try:
-    L.load_session_from_file(INSTAGRAM_USERNAME, SESSION_FILE)
-except Exception:
-    raise RuntimeError(
-        "Instagram session expired. Login locally and upload new session file."
-    )
+L.load_session_from_file(INSTAGRAM_USERNAME, SESSION_FILE)
 
-def fetch_posts(keyword, limit):
-    posts = []
+def fetch_posts(keyword, limit=5):
     hashtag = instaloader.Hashtag.from_name(L.context, keyword)
+    posts = []
 
     for post in hashtag.get_posts():
-        posts.append(f"https://www.instagram.com/p/{post.shortcode}/")
+        posts.append(post.shortcode)
         if len(posts) >= limit:
             break
 
